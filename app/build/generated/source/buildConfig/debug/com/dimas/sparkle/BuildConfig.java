9b4692c22e623ff367283a0f007bb534b09e1707
dimassatrio955 <83938358+dimassatrio955@users.noreply.github.com>
2022-06-09 18:12:16 +0700

    Dimas

---
+++ app/build/generated/source/buildConfig/debug/com/dimas/sparkle/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dimas.sparkle;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.dimas.sparkle";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
